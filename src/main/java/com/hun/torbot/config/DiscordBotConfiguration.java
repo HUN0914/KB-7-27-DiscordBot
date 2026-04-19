e47e9e9c2280b7ff418fe6647cae4e5b8563458b
bkh010914@naver.com <bkh010914@naver.com>
2026-04-19 09:16:39 +0900

    fix: 메시지 명령 인텐트 복구

--- src/main/java/com/hun/torbot/config/DiscordBotConfiguration.java
+++ src/main/java/com/hun/torbot/config/DiscordBotConfiguration.java
@@ -4,9 +4,7 @@ import com.hun.torbot.discord.TorDiscordListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,22 +15,12 @@ public class DiscordBotConfiguration {
     public JDA jda(DiscordBotProperties properties, TorDiscordListener listener) throws InterruptedException {
         return JDABuilder.createDefault(properties.botToken())
                 .enableIntents(
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT,
                         GatewayIntent.GUILD_MEMBERS,
                         GatewayIntent.GUILD_VOICE_STATES
                 )
-                .setChunkingFilter(ChunkingFilter.NONE)
                 .setMemberCachePolicy(MemberCachePolicy.VOICE)
-                .disableCache(
-                        CacheFlag.ACTIVITY,
-                        CacheFlag.CLIENT_STATUS,
-                        CacheFlag.EMOJI,
-                        CacheFlag.FORUM_TAGS,
-                        CacheFlag.ONLINE_STATUS,
-                        CacheFlag.ROLE_TAGS,
-                        CacheFlag.SCHEDULED_EVENTS,
-                        CacheFlag.STICKER,
-                        CacheFlag.VOICE_STATE
-                )
                 .addEventListeners(listener)
                 .build()
                 .awaitReady();
